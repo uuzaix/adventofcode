959de52525fa9d37ceb0c4fa6725ff1a5e35570f
Larisa Dolgova <larisa.dolgova@gmail.com>
2016-01-20 22:52:55 +0100

    Add brute force day_9 solution

---
+++ day_9.py
@@ -0,0 +1,26 @@
+import itertools
+
+with open('day_9.input') as file:
+	input = file.readlines()
+
+instructions = dict()
+places = []
+for line in input:
+	splited_line = line.split(' ')
+	instructions[(splited_line[0], splited_line[2])] = int(splited_line[4])
+	instructions[(splited_line[2], splited_line[0])] = int(splited_line[4])
+	places.append(splited_line[0])
+	places.append(splited_line[2])
+
+places  = set(places)
+orderings = itertools.permutations(places)
+
+
+def distance(orderings):
+	for e in orderings:
+		yield (instructions[(e[0], e[1])]+instructions[(e[1], e[2])]+instructions[(e[2], e[3])]+instructions[(e[3], e[4])]+instructions[(e[4], e[5])]
+			+instructions[(e[5], e[6])]+instructions[(e[6], e[7])])
+
+print min(distance(orderings))
+
+
