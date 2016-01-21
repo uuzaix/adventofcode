375b3389642667e951f1e5c3fdf233498e9ee549
Larisa Dolgova <larisa.dolgova@gmail.com>
2016-01-21 21:07:26 +0100

    Add day 9.2 solution

--- day_9.py
+++ day_9.py
@@ -12,15 +12,17 @@ for line in input:
 	places.append(splited_line[0])
 	places.append(splited_line[2])
 
-places  = set(places)
+places = set(places)
 orderings = itertools.permutations(places)
 
 
 def distance(orderings):
+	result = []
 	for e in orderings:
-		yield (instructions[(e[0], e[1])]+instructions[(e[1], e[2])]+instructions[(e[2], e[3])]+instructions[(e[3], e[4])]+instructions[(e[4], e[5])]
+		result.append(instructions[(e[0], e[1])]+instructions[(e[1], e[2])]+instructions[(e[2], e[3])]+instructions[(e[3], e[4])]+instructions[(e[4], e[5])]
 			+instructions[(e[5], e[6])]+instructions[(e[6], e[7])])
+	return min(result), max(result)
 
-print min(distance(orderings))
+print distance(orderings)
 
 
