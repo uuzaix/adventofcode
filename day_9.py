0b32980d77afef86917ff82d7d7425f240284174
Larisa Dolgova <larisa.dolgova@gmail.com>
2016-01-25 23:05:28 +0100

    Fix day9 solution - add multiple assignments and list comprehension

--- day_9.py
+++ day_9.py
@@ -6,11 +6,11 @@ with open('day_9.input') as file:
 instructions = dict()
 places = []
 for line in input:
-	splited_line = line.split(' ')
-	instructions[(splited_line[0], splited_line[2])] = int(splited_line[4])
-	instructions[(splited_line[2], splited_line[0])] = int(splited_line[4])
-	places.append(splited_line[0])
-	places.append(splited_line[2])
+	From, _, to, _, distance = line.split(' ')
+	instructions[(From, to)] = int(distance)
+	instructions[(to, From)] = int(distance)
+	places.append(From)
+	places.append(to)
 
 places = set(places)
 orderings = itertools.permutations(places)
@@ -19,8 +19,7 @@ orderings = itertools.permutations(places)
 def distance(orderings):
 	result = []
 	for e in orderings:
-		result.append(instructions[(e[0], e[1])]+instructions[(e[1], e[2])]+instructions[(e[2], e[3])]+instructions[(e[3], e[4])]+instructions[(e[4], e[5])]
-			+instructions[(e[5], e[6])]+instructions[(e[6], e[7])])
+		result.append(sum(instructions[(e[i], e[i+1])] for i in range(7)))
 	return min(result), max(result)
 
 print distance(orderings)
