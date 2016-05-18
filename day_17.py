35f8081242d74596fbe44edc76e2b2dd1b83002e
Larisa Dolgova <larisa.dolgova@gmail.com>
2016-05-18 17:54:36 +0200

    Improve day_17 solution

--- day_17.py
+++ day_17.py
@@ -2,20 +2,13 @@ import itertools
 
 containers = map(int, open('day_17.input').read().strip().split('\n'))
 
-total = 0
-
-min_number_of_cont = len(containers)
 comb_dict = {}
 for i in range(1, len(containers)):
-	comb_dict[i] = 0
 	for combination in itertools.combinations(containers, i):
 		if sum(combination) == 150:
-			total += 1
-			comb_dict[i] = comb_dict[i] + 1
+			comb_dict[i] = comb_dict.get(i, 0) + 1
 
-print "Total numbwr of combinations: " , total
+print "Total number of combinations: " , sum(comb_dict.values())
 
-for n, n_of_comb in comb_dict.iteritems():
-	if n == min(n for n, n_of_comb in comb_dict.iteritems() if n_of_comb > 0):
-		print "Number of combinations for min number of containers: " , n_of_comb
-
+min_length = min(comb_dict.keys())
+print "Number of combinations for min number of containers: ", comb_dict[min_length]
