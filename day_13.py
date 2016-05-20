ddac2a251d60a008079bb52ae19bb677312d42fb
Larisa Dolgova <larisa.dolgova@gmail.com>
2016-05-20 22:16:37 +0200

    Add part 2 solution for day_13

--- day_13.py
+++ day_13.py
@@ -15,15 +15,28 @@ for condition in input:
 
 orderings = itertools.permutations(persons)
 
-max_happy = 0
-for e in orderings:
-	happy = 0
-	for i in range(len(e)):
-		person1 = e[i]
-		person2 = e[i-1]
-		happy += relations[person1, person2]
-		happy += relations[person2, person1]
-	if max_happy < happy:
-		max_happy = happy
+def calculate_happy(orderings):
+	max_happy = 0
+	for e in orderings:
+		happy = 0
+		for i in range(len(e)):
+			person1 = e[i]
+			person2 = e[i-1]
+			happy += relations[person1, person2]
+			happy += relations[person2, person1]
+			if max_happy < happy:
+				max_happy = happy
+	return max_happy
 
-print max_happy
+print calculate_happy (orderings)
+
+for person in persons:
+	relations[person, 'me'] = 0
+	relations['me', person] = 0
+persons.add('me')
+
+orderings_with_me = itertools.permutations(persons)
+
+print calculate_happy (orderings_with_me)
+
+
