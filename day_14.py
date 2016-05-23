2b6c5ba80d2fa906dbbb0eb96b0eb6d457dea40d
Larisa Dolgova <larisa.dolgova@gmail.com>
2016-05-23 20:28:13 +0200

    Improve day_14 solution

--- day_14.py
+++ day_14.py
@@ -10,27 +10,24 @@ for condition in input:
 
 def find_distance(total_time):
 	result = {}
-	for key, param in conditions.iteritems()		:
-		speed, time, rest_time = param
-		name = key
+	for name, (speed, time, rest_time) in conditions.iteritems():
 		quotient = total_time // (time + rest_time)
 		remainder = total_time % (time + rest_time)
 		if remainder >= time:
 			result[name] = speed*(quotient+1)*time
 		else:
-			result[name] = int(speed)*quotient*int(time) + remainder*int(speed)
+			result[name] = speed*quotient*time + remainder*speed
 	max_dist = max(result.values())
-	return [{k:val} for k, val in result.items() if val == max_dist]
+	return [(k,val) for k, val in result.items() if val == max_dist]
 
-print find_distance(total_time)[0].values()[0]
+print find_distance(total_time)[0][1]
 
 
 def find_score(total_time):
 	scores = {}
 	for t in range(1, total_time + 1):
-		interm_result = find_distance(t)
-		for deer in interm_result:
-			scores[deer.keys()[0]] = scores.get(deer.keys()[0], 0) + 1
+		for name, _ in find_distance(t):
+			scores[name] = scores.get(name, 0) + 1
 	return max(scores.values())
 
 print find_score(total_time)
