a9989f7f31b264b25ff963f73a00557d241e4e17
Larisa Dolgova <larisa.dolgova@gmail.com>
2016-05-28 23:45:41 +0200

    Add day_20 part 2 solution

--- day_20.py
+++ day_20.py
@@ -9,12 +9,18 @@ def find_dividers(max):
 	return dividers
 
 def find_house(max):
+	part_1, part_2 = None, None
 	for house in xrange(10, 10000000, 10):
-		gifts = sum(find_dividers(house))
-		print (house, gifts)
-		if gifts*10 >= max:
-			return house
-			break
-
+		dividors = find_dividers(house)
+		if not part_1:
+			gifts1 = sum(dividors)
+			if gifts1*10 >= max:
+				part_1 = house
+		if not part_2:
+			gifts2 = sum(d for d in dividors if house/d <= 50)
+			if gifts2*11 >= max:
+				part_2 = house
+				break
+	return part_1, part_2
 
 print find_house(34000000)
