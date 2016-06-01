6fa86a8762bad4bc00cc134cb5fdac884c4366f8
Larisa Dolgova <larisa.dolgova@gmail.com>
2016-06-01 21:12:28 +0200

    Add part2 day_19 solution

--- day_19.py
+++ day_19.py
@@ -1,15 +1,35 @@
-import itertools
-import re
+from random import shuffle
 
 inputs = map(str, open('day_19.input').read().strip().split('\n'))
 molec = inputs[-1]
+repos = []
 
 all_molec = set ()
 for line in inputs[:-2]:
 	old, _, new = line.split(" ")
+	repos.append((old, new))
 	for ind in range(len(molec)):
 		if molec[ind:ind+len(old)] == old:
 			molec_new = molec[:ind] + new + molec[ind+len(old):]
 			all_molec.add(molec_new)
 
 print len(all_molec)
+
+# part2
+target = molec
+count = 0
+
+while target != 'e':
+	tmp = target
+	for old, new in repos:
+		if new not in target:
+			continue
+		else:
+			target = target.replace(new, old, 1)
+			count += 1
+	if tmp == target:
+		target = molec
+		count = 0
+		shuffle(repos)
+
+print count
